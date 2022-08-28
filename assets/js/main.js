d59d8e916f3ef4d4d9d2111defcb7e7a1da75f99
TurksoyAz <supercoder@AirvonSperCoder.fritz.box>
2022-08-28 17:25:30 +0200

    lev1_1 forms+js_function-double

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+
+
+let adress=document.getElementById("paragraf");
+
+
+ function button(){
+
+    let a = document.getElementById("say1").value;
+    let b = a * 2
+    paragraf.innerHTML=b
+
+ }
+
+
+
+
